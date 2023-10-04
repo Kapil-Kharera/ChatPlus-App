ae73a4ed5c7cb5ff7adb18c2f4e73b98d495d818
Kapil Kharera <kapil95881@gmail.com>
2023-10-04 18:03:16 +0530

    Connected to mongodb, handle error and add debug mode

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,29 @@
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import app from "./app.js";
 import logger from "./config/logger.config.js";
 
 //config dotenv
 dotenv.config();
+
+//exit on db error
+mongoose.connection.on("error", (err) => {
+    logger.error(`Mongodb connection error: ${err}`);
+    process.exit(1);
+});
+
+// db debug mode
+if(process.env.NODE_ENV !== "production") {
+    mongoose.set("debug", true);
+}
+
+//database connection
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    logger.info("Connected to Mongodb");
+})
 
 const server = app.listen(process.env.PORT || 8000, () => {
     logger.info(`Server is listening on ${process.env.PORT}`);
