47767c20f27b42d92a4825cacb4d385f06513356
Kapil Kharera <kapil95881@gmail.com>
2023-10-09 13:27:16 +0530

    Add custom input fields & finish the form style

--- frontend/src/components/auth/RegisterForm.js
+++ frontend/src/components/auth/RegisterForm.js
@@ -1,8 +1,13 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { signUpSchema } from "../../utils/validation";
+import { useSelector } from "react-redux";
+import  PulseLoader  from "react-spinners/PulseLoader";
+import { Link } from 'react-router-dom';
+import AuthInput from "./AuthInput";
 
 const RegisterForm = () => {
+  const { status } = useSelector((state) => state.user);
   const { 
     register, 
     handleSubmit, 
@@ -12,8 +17,6 @@ const RegisterForm = () => {
   });
   const onSubmit = (data) => console.log(data);
 
-  console.log("Values : ", watch());
-  console.log("Errors : ", errors);
   return (
     <div className="h-screen w-full flex items-center justify-center overflow-hidden">
         {/* container */}
@@ -28,8 +31,50 @@ const RegisterForm = () => {
               className="mt-6 space-y-6"
               onSubmit={handleSubmit(onSubmit)}
               >
-                <input type="text" {...register("name")} />
-                <button type="submit">Submit</button>
+                <AuthInput 
+                  name="name"
+                  type="text"
+                  placeholder="Name"
+                  register={register}
+                  error={errors?.name?.message}
+                  />
+
+                <AuthInput 
+                  name="email"
+                  type="text"
+                  placeholder="Email"
+                  register={register}
+                  error={errors?.email?.message}
+                  />
+
+                <AuthInput 
+                  name="status"
+                  type="text"
+                  placeholder="Status"
+                  register={register}
+                  error={errors?.status?.message}
+                  />
+
+                <AuthInput 
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  register={register}
+                  error={errors?.password?.message}
+                  />
+                  {/* Submit Button */}
+                <button 
+                className="w-full flex justify-center bg-green_1 text-gray-100 p-4 rounded-full tracking-wide font-semibold foucs:outline-none hover:bg-green_2 shadow-lg cursor-pointer transition ease-in duration-300"
+                type="submit">
+                  { status === "loading" ? <PulseLoader color="#fff" size={16} /> : "Sign up"}
+                </button>
+                {/* sign in link */}
+                <p className="flex flex-col items-center justify-center mt-10 text-center text-md dark:text-dark_text_1">
+                  <span>have an account ?</span>
+                  <Link 
+                    to="/login"
+                    className="hover:underline cursor-pointer transition ease-in duration-300">Sign in</Link>
+                </p>
             </form>
         </div> 
     </div>
