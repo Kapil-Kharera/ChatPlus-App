5c8db40c079535e8df97c64671c0666ec210496e
Kapil Kharera <kapil95881@gmail.com>
2023-10-17 10:49:49 +0530

    Added layout for sidebar notification

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -1,10 +1,13 @@
 import { SidebarHeader } from "./header";
+import { Notification } from "./notifications"
 
 export default function Sidebar() {
   return (
     <div className="w-[40%] h-full select-none">
         {/* sidebar header */}
         <SidebarHeader />
+        {/* notification */}
+        <Notification />
     </div>
   )
 }
