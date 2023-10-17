33e677f02663fabbc9b72d0600f2adf1f5fa28fa
Kapil Kharera <kapil95881@gmail.com>
2023-10-17 12:36:41 +0530

    Added sidebar search component

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import { SidebarHeader } from "./header";
 import { Notification } from "./notifications"
+import { Search } from "./search";
 
 export default function Sidebar() {
+  const [ searchResults, setSearchResults] = useState([]);
   return (
     <div className="w-[40%] h-full select-none">
         {/* sidebar header */}
         <SidebarHeader />
         {/* notification */}
         <Notification />
+        {/* search */}
+        <Search searchLength={searchResults.length} />
     </div>
   )
 }
